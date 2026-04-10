7a682546ba4740f41e084607db9489c772328d7c
Faisal <kucingbau074@gmail.com>
2026-04-10 20:53:19 +0800

    Add files via upload

---
+++ Modul 1/PRAK101.php
@@ -0,0 +1,7 @@
+<?php
+$Nama = "Faisal Tanjung";
+$NIM = "2410817310012";
+echo "Hello World in PHP<br>";
+echo "Nama Praktikan : $Nama<br>";
+echo "NIM : $NIM<br>";
+?>
---
+++ Modul 1/PRAK102.php
@@ -0,0 +1,7 @@
+<?php
+$Jari_jari = 4.2;
+$Tinggi = 5.4;
+$Volume = round(1/3 * pi() * ($Jari_jari * $Jari_jari) * $Tinggi, 3);
+
+echo ("$Volume m3")
+?>
